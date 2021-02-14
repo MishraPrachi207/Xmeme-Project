4a312955ea28c6659871412b93117a732f1fa84e
Prachi  Mishra <prakkymishra20@gmail.com>
2021-02-14 21:29:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,19 @@
 
+sudo apt-get update -y
+sudo apt-get install -y mysql-server-5.7
+sudo mysql
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'Password@123!';
+GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+exit
+sudo apt install openjdk-8-jre-headless -y
+sudo apt-get install openjdk-8-jdk -y	
+sudo apt-get install maven -y
+sudo apt install maven -y
+mvn clean package
+sudo apt update
+sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-
+
+certificates
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt -y install nodejs
