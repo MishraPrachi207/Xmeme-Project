2d2397efdb8bb59917129eb3d2e3f312df42e8d9
Prachi  Mishra <prakkymishra20@gmail.com>
2021-02-14 22:38:37 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sudo apt-get install -y mysql-server-5.7
 sudo mysql -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'Password@123'"
 sudo mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' WITH GRANT OPTION"
 sudo mysql -e "FLUSH PRIVILEGES"
+sudo mysql -e "CREATE DATABSE xmemedb"
 sudo apt install openjdk-8-jre-headless -y
 sudo apt-get install openjdk-8-jdk -y	
 sudo apt-get install maven -y
