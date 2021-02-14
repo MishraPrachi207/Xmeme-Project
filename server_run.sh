5e04dd2481129dd136155bdd3159c528dd80c368
Prachi  Mishra <prakkymishra20@gmail.com>
2021-02-14 21:40:42 +0530

    Update server_run.sh

--- server_run.sh
+++ server_run.sh
@@ -1,1 +1,8 @@
+cd Xmeme-project\demo
+mvn clean 
+java -jar "Xmeme-project\demo\target\xmeme-backend-docker.jar"
 
+cd Xmeme-project\xmeme-frontend
+npm install
+npm audit fix --force
+npm run build
