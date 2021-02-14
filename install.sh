d0a6bfc358cb6432e4af5919b4b48f2c3c41bac5
Prachi  Mishra <prakkymishra20@gmail.com>
2021-02-14 21:29:31 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,6 @@ sudo apt-get install maven -y
 sudo apt install maven -y
 mvn clean package
 sudo apt update
-sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-
-
-certificates
+sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt -y install nodejs
