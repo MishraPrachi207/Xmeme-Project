a17bd1c2cf35e16b7eaa31c326fb225204df2715
Prachi  Mishra <prakkymishra20@gmail.com>
2021-02-15 11:19:25 +0530

    Update server_run.sh

--- server_run.sh
+++ server_run.sh
@@ -1,5 +1,3 @@
-sudo mvn -f demo/pom.xml clean package
-java -jar "Xmeme-project\demo\target\xmeme-backend-docker.jar"
-npm --prefix ./xmeme-frontend install
-npm --prefix ./xmeme-frontend audit fix --force
-npm --prefix ./xmeme-frontend run build
+java -jar "./demo/target/xmeme-backend-docker.jar" &
+npm --prefix ./xmeme-frontend start
+
