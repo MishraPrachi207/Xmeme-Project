aaa4a33ae0b873d6b27cbf4317f8b93e0e01441f
Prachi  Mishra <prakkymishra20@gmail.com>
2021-02-15 11:19:29 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,3 +12,7 @@ sudo apt update
 sudo apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt -y install nodejs
+sudo mvn -f demo/pom.xml clean package
+npm --prefix ./xmeme-frontend install
+npm --prefix ./xmeme-frontend audit fix --force
+npm --prefix ./xmeme-frontend run build
